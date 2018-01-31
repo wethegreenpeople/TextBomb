bf58877396b18224743583b4b0281b0b2f205e38
John Singh <wethegreenpeople@gmail.com>
2018-01-30 21:57:59 -0800

    Fixed bomb defusal. TextBomb can be stopped by replying with a keyword

--- app/src/main/java/xyz/uraqt/apps/annoy/SmsListener.java
+++ app/src/main/java/xyz/uraqt/apps/annoy/SmsListener.java
@@ -9,17 +9,14 @@ import android.widget.Toast;
 
 
 public class SmsListener extends BroadcastReceiver {
+    static String messageBody = "";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        String messageBody = "";
+        String bombDefuse = MainActivity.bombDefuse;
         if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
             for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
                 messageBody = smsMessage.getMessageBody();
-
-                Toast toast = Toast.makeText(context, messageBody, Toast.LENGTH_LONG);
-                toast.show();
             }
         }
     }
